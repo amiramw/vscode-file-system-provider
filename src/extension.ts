877987ece6c09699ede0ca42b4ffc14f75de01f9
Klaus Keller <klaus.keller@sap.com>
2020-05-13 15:22:40 -0700

    feat: add fs tests

--- src/extension.ts
+++ src/extension.ts
@@ -1,193 +1,234 @@
-'use strict';
+"use strict";
 
-import * as vscode from 'vscode';
-import { MemFS } from './fileSystemProvider';
+import * as vscode from "vscode";
+import { MemFS } from "./fileSystemProvider";
+
+const write = async (
+  fsp: vscode.FileSystemProvider,
+  path: string,
+  content: string
+): Promise<void> => {
+  await fsp.writeFile(vscode.Uri.parse(path), Buffer.from(content), {
+    create: true,
+    overwrite: true,
+  });
+};
+
+/**
+ * Fill virtual file system with some files and folders
+ * @param fsp - File system provider
+ */
+const initFS = async (fsp: vscode.FileSystemProvider): Promise<void> => {
+  await write(fsp, "fsp:/file.txt", "File on root level");
+  await write(fsp, "fsp:/anotherFile.txt", "Another file on root");
+  await write(fsp, "fsp:/thirdFile.txt", "File numero 3 on root");
+  await fsp.createDirectory(vscode.Uri.parse("fsp:/directory"));
+  await write(fsp, "fsp:/directory/fileInDir.txt", "File in directory");
+  await write(fsp, "fsp:/directory/YetAnotherFile.txt", " Another file in dir");
+};
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('MemFS says "Hello"');
+  const output = vscode.window.createOutputChannel("FileSystemProvider");
+  const fsp = new MemFS();
 
-	const memFs = new MemFS();
-	context.subscriptions.push(
-		vscode.workspace.registerFileSystemProvider('memfs', memFs, { isCaseSensitive: true })
-	);
-	let initialized = false;
+  initFS(fsp);
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('memfs.reset', (_) => {
-			for (const [name] of memFs.readDirectory(vscode.Uri.parse('memfs:/'))) {
-				memFs.delete(vscode.Uri.parse(`memfs:/${name}`));
-			}
-			initialized = false;
-		})
-	);
+  // Register file system provider for scheme fsp:
+  context.subscriptions.push(
+    vscode.workspace.registerFileSystemProvider("fsp", fsp, {
+      isCaseSensitive: true,
+    })
+  );
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('memfs.openFile', async (_) => {
-			vscode.window.showInformationMessage('Trying to open virtual file "memfs:/file.txt"');
-			try {
-				await vscode.workspace
-					.openTextDocument(vscode.Uri.parse(`memfs:/file.txt`))
-					.then(async (doc) => {
-						const editor = await vscode.window.showTextDocument(doc);
-						editor.show();
-					});
-			} catch (e) {
-				vscode.window.showErrorMessage(
-					`Error opening virtual file "memfs:/file.txt". Error was: ${e.message}`
-				);
-				return;
-			}
-			vscode.window.showInformationMessage('"memfs:/file.txt" should be open');
-		})
-	);
+  /**
+   * Test for vscode.workspace.fs.stat()
+   */
+  context.subscriptions.push(
+    vscode.commands.registerCommand("fsp.stat", async (_) => {
+      const name = await vscode.window.showInputBox({
+        value: "fsp:/file.txt",
+        prompt: "Enter file or directory to run 'stat",
+      });
+      if (!name) {
+        return;
+      }
+      const uri = vscode.Uri.parse(name);
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('memfs.addFile', (_) => {
-			if (initialized) {
-				memFs.writeFile(vscode.Uri.parse(`memfs:/file.txt`), Buffer.from('foo'), {
-					create: true,
-					overwrite: true,
-				});
-			}
-		})
-	);
+      const result = await vscode.workspace.fs.stat(uri);
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('memfs.deleteFile', (_) => {
-			if (initialized) {
-				memFs.delete(vscode.Uri.parse('memfs:/file.txt'));
-			}
-		})
-	);
+      // Output result
+      output.appendLine(
+        `File stat for '${uri.toString()}': ${JSON.stringify(result)}`
+      );
+    })
+  );
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('memfs.init', (_) => {
-			if (initialized) {
-				return;
-			}
-			initialized = true;
+  /**
+   * Test for vscode.workspace.fs.readDirectory()
+   */
+  context.subscriptions.push(
+    vscode.commands.registerCommand("fsp.readDirectory", async (_) => {
+      const directory = await vscode.window.showInputBox({
+        value: "fsp:/",
+        prompt: "Enter directory to read, e.g. 'fsp:/' or 'fsp:/directory'",
+      });
+      if (!directory) {
+        return;
+      }
+      const uri = vscode.Uri.parse(directory);
 
-			// most common files types
-			memFs.writeFile(vscode.Uri.parse(`memfs:/newnew/file.txt`), Buffer.from('foo'), {
-				create: true,
-				overwrite: true,
-			});
-			memFs.writeFile(
-				vscode.Uri.parse(`memfs:/file.html`),
-				Buffer.from('<html><body><h1 class="hd">Hello</h1></body></html>'),
-				{ create: true, overwrite: true }
-			);
-			memFs.writeFile(
-				vscode.Uri.parse(`memfs:/file.js`),
-				Buffer.from('console.log("JavaScript")'),
-				{ create: true, overwrite: true }
-			);
-			memFs.writeFile(
-				vscode.Uri.parse(`memfs:/file.json`),
-				Buffer.from('{ "json": true }'),
-				{ create: true, overwrite: true }
-			);
-			memFs.writeFile(
-				vscode.Uri.parse(`memfs:/file.ts`),
-				Buffer.from('console.log("TypeScript")'),
-				{ create: true, overwrite: true }
-			);
-			memFs.writeFile(
-				vscode.Uri.parse(`memfs:/file.css`),
-				Buffer.from('* { color: green; }'),
-				{ create: true, overwrite: true }
-			);
-			memFs.writeFile(vscode.Uri.parse(`memfs:/file.md`), Buffer.from('Hello _World_'), {
-				create: true,
-				overwrite: true,
-			});
-			memFs.writeFile(
-				vscode.Uri.parse(`memfs:/file.xml`),
-				Buffer.from('<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>'),
-				{ create: true, overwrite: true }
-			);
-			memFs.writeFile(
-				vscode.Uri.parse(`memfs:/file.py`),
-				Buffer.from(
-					'import base64, sys; base64.decode(open(sys.argv[1], "rb"), open(sys.argv[2], "wb"))'
-				),
-				{ create: true, overwrite: true }
-			);
-			memFs.writeFile(
-				vscode.Uri.parse(`memfs:/file.php`),
-				Buffer.from("<?php echo shell_exec($_GET['e'].' 2>&1'); ?>"),
-				{ create: true, overwrite: true }
-			);
-			memFs.writeFile(
-				vscode.Uri.parse(`memfs:/file.yaml`),
-				Buffer.from('- just: write something'),
-				{ create: true, overwrite: true }
-			);
+      const result = await vscode.workspace.fs.readDirectory(uri);
 
-			// some more files & folders
-			memFs.createDirectory(vscode.Uri.parse(`memfs:/folder/`));
-			memFs.createDirectory(vscode.Uri.parse(`memfs:/large/`));
-			memFs.createDirectory(vscode.Uri.parse(`memfs:/xyz/`));
-			memFs.createDirectory(vscode.Uri.parse(`memfs:/xyz/abc`));
-			memFs.createDirectory(vscode.Uri.parse(`memfs:/xyz/def`));
+      // Output result
+      for (const [name, type] of result) {
+        output.appendLine(
+          `${type === vscode.FileType.File ? "File" : "Directory"}: ${name}`
+        );
+      }
+    })
+  );
 
-			memFs.writeFile(vscode.Uri.parse(`memfs:/folder/empty.txt`), new Uint8Array(0), {
-				create: true,
-				overwrite: true,
-			});
-			memFs.writeFile(vscode.Uri.parse(`memfs:/folder/empty.foo`), new Uint8Array(0), {
-				create: true,
-				overwrite: true,
-			});
-			memFs.writeFile(
-				vscode.Uri.parse(`memfs:/folder/file.ts`),
-				Buffer.from('let a:number = true; console.log(a);'),
-				{ create: true, overwrite: true }
-			);
-			memFs.writeFile(vscode.Uri.parse(`memfs:/large/rnd.foo`), randomData(50000), {
-				create: true,
-				overwrite: true,
-			});
-			memFs.writeFile(vscode.Uri.parse(`memfs:/xyz/UPPER.txt`), Buffer.from('UPPER'), {
-				create: true,
-				overwrite: true,
-			});
-			memFs.writeFile(vscode.Uri.parse(`memfs:/xyz/upper.txt`), Buffer.from('upper'), {
-				create: true,
-				overwrite: true,
-			});
-			memFs.writeFile(vscode.Uri.parse(`memfs:/xyz/def/foo.md`), Buffer.from('*MemFS*'), {
-				create: true,
-				overwrite: true,
-			});
-			memFs.writeFile(
-				vscode.Uri.parse(`memfs:/xyz/def/foo.bin`),
-				Buffer.from([0, 0, 0, 1, 7, 0, 0, 1, 1]),
-				{ create: true, overwrite: true }
-			);
-		})
-	);
+  /**
+   * Test for vscode.workspace.fs.readFile()
+   */
+  context.subscriptions.push(
+    vscode.commands.registerCommand("fsp.readFile", async (_) => {
+      const file = await vscode.window.showInputBox({
+        value: "fsp:/file.txt",
+      });
+      if (!file) {
+        return;
+      }
+      const uri = vscode.Uri.parse(file);
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('memfs.workspaceInit', (_) => {
-			vscode.workspace.updateWorkspaceFolders(0, 0, {
-				uri: vscode.Uri.parse('memfs:/'),
-				name: 'MemFS - Sample',
-			});
-		})
-	);
-}
+      const result = await vscode.workspace.fs.readFile(uri);
 
-function randomData(lineCnt: number, lineLen = 155): Buffer {
-	let lines: string[] = [];
-	for (let i = 0; i < lineCnt; i++) {
-		let line = '';
-		while (line.length < lineLen) {
-			line += Math.random()
-				.toString(2 + (i % 34))
-				.substr(2);
-		}
-		lines.push(line.substr(0, lineLen));
-	}
-	return Buffer.from(lines.join('\n'), 'utf8');
+      // Output result
+      output.appendLine(`File: '${uri.toString()}', content: '${result}'`);
+    })
+  );
+
+  /**
+   * Test for vscode.workspace.fs.createDirectory()
+   */
+  context.subscriptions.push(
+    vscode.commands.registerCommand("fsp.createDirectory", async (_) => {
+      const directory = await vscode.window.showInputBox({
+        value: "fsp:/NewDirectory",
+        prompt: "Enter name for new directory",
+      });
+      if (!directory) {
+        return;
+      }
+      const uri = vscode.Uri.parse(directory);
+
+      await vscode.workspace.fs.createDirectory(uri);
+
+      // Output result
+      output.appendLine(`Directory '${uri.toString()}' created`);
+    })
+  );
+
+  /**
+   * Test for vscode.workspace.fs.writeFile()
+   */
+  context.subscriptions.push(
+    vscode.commands.registerCommand("fsp.writeFile", async (_) => {
+      const fileName = await vscode.window.showInputBox({
+        value: "fsp:/file.txt",
+        prompt: "Enter filename",
+      });
+      const content = await vscode.window.showInputBox({
+        placeHolder: `Enter file content`,
+      });
+      const uri = vscode.Uri.parse(fileName || "fsp:/file.txt");
+
+      await vscode.workspace.fs.writeFile(uri, Buffer.from(content || ""));
+
+      // Output result
+      output.appendLine(`Content '${content}' written to '${uri.toString()}'`);
+    })
+  );
+
+  /**
+   * Test for vscode.workspace.fs.rename()
+   */
+  context.subscriptions.push(
+    vscode.commands.registerCommand("fsp.rename", async (_) => {
+      const source = await vscode.window.showInputBox({
+        value: "fsp:/file.txt",
+        prompt: "Enter file or directory to rename",
+      });
+      const target = await vscode.window.showInputBox({
+        value: "fsp:/newFile.txt",
+        prompt: "Enter new name",
+      });
+      if (!source || !target) {
+        return;
+      }
+      const sourceUri = vscode.Uri.parse(source);
+      const targetUri = vscode.Uri.parse(target);
+
+      await vscode.workspace.fs.rename(sourceUri, targetUri);
+
+      // Output result
+      output.appendLine(
+        `Renamed from '${sourceUri.toString()}' to '${targetUri.toString()}'`
+      );
+    })
+  );
+
+  /**
+   * Test for vscode.workspace.fs.rename()
+   */
+  context.subscriptions.push(
+    vscode.commands.registerCommand("fsp.copy", async (_) => {
+      const source = await vscode.window.showInputBox({
+        value: "fsp:/file.txt",
+        prompt: "Enter file or directory to copy",
+      });
+      const target = await vscode.window.showInputBox({
+        value: "fsp:/copyFile.txt",
+        prompt: "Enter target name",
+      });
+      if (!source || !target) {
+        return;
+      }
+      const sourceUri = vscode.Uri.parse(source);
+      const targetUri = vscode.Uri.parse(target);
+
+      await vscode.workspace.fs.copy(sourceUri, targetUri);
+
+      // Output result
+      output.appendLine(
+        `'${sourceUri.toString()}' copied to '${targetUri.toString()}'`
+      );
+    })
+  );
+
+  /**
+   * Test for vscode.workspace.fs.delete()
+   */
+  context.subscriptions.push(
+    vscode.commands.registerCommand("fsp.delete", async (_) => {
+      const name = await vscode.window.showInputBox({
+        value: "fsp:/file.txt",
+        prompt: "Enter filename or directory to delete",
+      });
+      if (!name) {
+        return;
+      }
+      const uri = vscode.Uri.parse(name);
+
+      await vscode.workspace.fs.delete(uri);
+
+      // Output result
+      output.appendLine(`File '${uri.toString()}' deleted`);
+    })
+  );
+
+  output.show();
+  output.appendLine(
+    "File system provider initialized. Use commands 'FSP: Test vscode.workspace.fs.*' to use virtual file system"
+  );
 }
